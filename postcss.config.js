a75d03d87b2494ba864248ad7a771f99062ba36a
chillchill011 <chill.panels.0r@icloud.com>
2025-02-25 14:34:59 +0530

    Netlify copy

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,11 @@
-console.log('ELEVENTY_ENV:', process.env.ELEVENTY_ENV);
-console.log('NODE_ENV:', process.env.NODE_ENV);
-
 module.exports = {
   plugins: [
     require('postcss-import'),
     require('tailwindcss/nesting'),
     require('tailwindcss'),
     require('autoprefixer'),
-    require('cssnano')({
+    process.env.NODE_ENV === 'production' && require('cssnano')({
       preset: 'default'
     })
-  ]
+  ].filter(Boolean)
 };
