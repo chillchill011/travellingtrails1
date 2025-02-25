2639d243f8f764ffef516f8d73fdd47ae945777b
chillchill011 <chill.panels.0r@icloud.com>
2025-02-25 11:19:30 +0530

    Error resolving css

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,13 @@
+console.log('ELEVENTY_ENV:', process.env.ELEVENTY_ENV);
+console.log('NODE_ENV:', process.env.NODE_ENV);
+
 module.exports = {
   plugins: [
     require('postcss-import'),
     require('tailwindcss/nesting'),
     require('tailwindcss'),
     require('autoprefixer'),
-    process.env.NODE_ENV === 'production' && require('cssnano')({
+    process.env.ELEVENTY_ENV === 'production' && require('cssnano')({
       preset: 'default'
     })
   ].filter(Boolean)
